bf48d6cdfa9df5084daf1f8f4f10aa956dada572
Quin Nordmark <quinnordmark@gmail.com>
2024-01-17 00:33:58 -0700

    User story 6 complete

--- spec/features/merchant/coupons/index_spec.rb
+++ spec/features/merchant/coupons/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Coupon Index", type: :feature do
   before(:each) do
     @merchant_1 = Merchant.create(name: "Chucky Cheese")
     @coupon_1 = @merchant_1.coupons.create(name: "$10 off", code: "10off", dollar_off: 10)
-    @coupon_2 = @merchant_1.coupons.create(name: "5% off", code: "5off", percent_off: 5)
+    @coupon_2 = @merchant_1.coupons.create(name: "5% off", code: "5off", percent_off: 5, status: 1)
   end 
 
   it "1. Merchant Coupons Index" do 
@@ -24,4 +24,17 @@ RSpec.describe "Coupon Index", type: :feature do
     expect(page).to have_link(@coupon_1.name)
     expect(page).to have_link(@coupon_2.name)
   end
+
+  it "6. Merchant Coupon Index Sorted" do
+    # When I visit my coupon index page
+    visit "/merchants/#{@merchant_1.id}/coupons"
+    # I can see that my coupons are separated between active and inactive coupons. 
+    within ".activated-coupons" do
+      expect(page).to have_content(@coupon_1.name)
+    end
+    within ".deactivated-coupons" do
+      expect(page).to have_content(@coupon_2.name)
+    end
+  end
+
 end 
